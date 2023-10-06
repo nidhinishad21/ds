72fa575b52d6e82a9e8a7f94c09cd65467d2e527
nidhinishad21 <nidhinishad21@gmail.com>
2023-10-06 00:09:03 -0700

    Adding first commit

---
+++ MyArrayList/src/MyArrayList.java
@@ -0,0 +1,62 @@
+
+public class MyArrayList {
+	private int[] arr;
+	private int count;
+	private int index;
+	
+	public int getIndex() {
+		return index;
+	}
+	
+	public MyArrayList() {
+		count=0;
+		index=0;
+		arr = new int[10];
+	}
+	/**
+	 * 
+	 * @param input
+	 */
+	public void add(int input) {
+		if(count>arr.length) {
+			//
+			int[] arr2 = new int[arr.length*2];
+			copyIntoNewArr(arr, arr2);
+		}
+		arr[index]=input;
+		index++;
+		count++;
+		
+	}
+	
+	private void copyIntoNewArr(int[] oldArr, int[] newArr) {
+		
+		for(int i=0; i<oldArr.length; i++) {
+			newArr[i]=oldArr[i];
+		}
+		
+	}
+	
+	public void print() {
+		for (int i=0; i<index;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+	
+	public int size() {
+		return count;
+		//
+	}
+	/*
+	public void update(int numU, int pos) {
+		
+	}
+	public void delete(int ) {
+		
+	}
+	public void retrieve() {
+		
+	}
+	*/
+
+}
---
+++ MyArrayList/src/Program.java
@@ -0,0 +1,18 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MyArrayList a1 = new MyArrayList();
+		
+		a1.add(2);
+		a1.add(3);
+		
+		a1.print();// 2 3
+		System.out.println(a1.size());
+		//a1.size();
+
+	}
+
+}
